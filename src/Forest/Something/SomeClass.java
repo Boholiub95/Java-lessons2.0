229070834f3e297b0bf8db185c233ef4f512d8e7
Oleh Boholiub@ <boholiub95@gmail.com>
2021-10-17 09:14:51 -0600

    TypeOFpackage

---
+++ src/Forest/Something/SomeClass.java
@@ -0,0 +1,7 @@
+package Forest.Something;
+
+public class SomeClass {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,17 @@
+import Forest.Something.SomeClass;
+import Forest.Squirell;
+import Forest.Tree;
+import java.util.Scanner;
+import Forest.*;
+
+public class Test {
+    public static void main(String[] args) {
+        /*Tree tree1 = new Tree();
+        Squirell squirell1 = new Squirell();*
+         */
+        Scanner scanner = new Scanner(System.in);
+        SomeClass someClass1 = new SomeClass();
+
+
+    }
+}
