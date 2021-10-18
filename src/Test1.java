51900a12821406f8a0f60eb12657833383cea864
Oleh Boholiub@ <boholiub95@gmail.com>
2021-10-18 07:26:51 -0600

    public, private, protected и default

---
+++ src/Test1.java
@@ -0,0 +1,10 @@
+import Package1.Person;
+
+public class Test1 {
+    public static void main(String[] args) {
+        // privat, public,default(),protected.
+        Person person1 = new Person();
+        person1.name ="Bob";
+
+    }
+}
