d4fb210b0769ab433018721108a8542534c3b7cf
Oleh Boholiub@ <boholiub95@gmail.com>
2021-10-20 17:35:40 -0600

    Wrapper classes for primitive data types.

---
+++ src/Test5.java
@@ -0,0 +1,8 @@
+public class Test5 {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(x);
+        Integer x2 = new Integer(123);
+        int i = Integer.parseInt(0.254);
+    }
+}
