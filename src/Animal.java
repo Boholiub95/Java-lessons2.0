5977bb47d60f9e8a3cd5ff62e16ba7ed1a5a5ebd
Oleh Boholiub@ <boholiub95@gmail.com>
2021-10-23 05:53:56 -0600

    Anonymous classes

--- src/Animal.java
+++ src/Animal.java
@@ -1,5 +1,22 @@
 public class Animal {
-    public void eat () {
+    public void eat() {
         System.out.println("I am eating....");
+    }
+class OtherAnimal extends Animal{
+    @Override
+    public void eat() {
+        System.out.println("Other animal is eating");
+    }
+
+
+}
+
+    public class Test {
+        public static void main(String[] args) {
+            Animal animal = new Animal();
+            animal.eat();
+           /* OtherAnimal otherAnimal = new OtherAnimal();
+            otherAnimal.eat();*/
+        }
     }
 }
