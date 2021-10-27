a7a76d877bc2802fb9f229c4512bf3fdc2ddd8b5
Oleh Boholiub@ <boholiub95@gmail.com>
2021-10-27 06:59:39 -0600

    rolemai

--- src/Task3.java
+++ src/Task3.java
@@ -13,4 +13,29 @@ public  class Task3 {
                     System.out.println("admin controls everything");
             }
         }
+    }
 }
+/*
+public enum Role {
+ GUEST, CLIENT, MODERATOR, ADMIN
+}
+
+public class RoleMain {
+ public static void main(String[] args) {
+ Role role = Role.valueOf("Admin".toUpperCase());
+ switch (role){
+ case GUEST:
+ System.out.println(role + " can only watch");
+ case CLIENT:
+ System.out.println(role + " can place orders");
+ case MODERATOR:
+ System.out.println(role + " can manage his section");
+JAVA FROM EPAM
+98
+ case ADMIN:
+ System.out.println(role + " controls everything");
+ }
+ }
+}
+ */
+
