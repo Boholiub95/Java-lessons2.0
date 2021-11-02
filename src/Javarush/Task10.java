f319b3a163249453d5b87b648ff7100a98dfddeb
Oleh Boholiub@ <boholiub95@gmail.com>
2021-11-02 05:43:58 -0600

    Task10

---
+++ src/Javarush/Task10.java
@@ -0,0 +1,17 @@
+package Javarush;
+
+/*
+Рисуем треугольник
+*/
+
+public class Task10 {
+    public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j <= i; j++) {
+            System.out.print('8');
+        }
+
+        System.out.println();
+    }
+}
+}
