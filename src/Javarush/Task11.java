03974b18f112ae1e08707c87993636857efc05db
Oleh Boholiub@ <boholiub95@gmail.com>
2021-11-02 05:49:26 -0600

    Task11

---
+++ src/Javarush/Task11.java
@@ -0,0 +1,22 @@
+package Javarush;
+import java.util.Scanner;
+
+/*
+Хорошего не бывает много
+*/
+
+
+public class Task11 {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String string = scanner.nextLine();
+    int number = scanner.nextInt();
+
+    do {
+        System.out.println(string);
+        number--;
+    } while (number > 0 && number < 4);
+}
+}
+
+}
