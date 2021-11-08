d72440afe9776b36e2e33f8e5008dbe0614ece33
Oleh Boholiub@ <boholiub95@gmail.com>
2021-11-08 08:20:28 -0500

    ArraysToString

---
+++ src/Javarush/ArrayToString.java
@@ -0,0 +1,16 @@
+package Javarush;
+
+import java.util.Arrays;
+
+
+
+public class ArrayToString {
+    public static String[] strings = new String[]{"I", "love", "Java"};
+    public static int[] ints = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.toString(ints));
+    }
+}
+
