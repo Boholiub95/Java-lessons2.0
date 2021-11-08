1d650e0213d8abeb40255a6f901a1618eb1eda68
Oleh Boholiub@ <boholiub95@gmail.com>
2021-11-08 05:50:57 -0700

    Solution9

---
+++ src/Javarush/Solution9.java
@@ -0,0 +1,26 @@
+package Javarush;
+
+import java.util.Scanner;
+
+/*
+Создаем двумерный массив
+Reads from the console the number N - the number of lines in the array (consider that this number will be greater than 0, you can skip checking).
+Reads N numbers from the console (you don't have to check any numbers that are greater than 0).
+Initializes a two-dimensional multiArray:
+*/
+
+public class Solution {
+    public static int[][] multiArray;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        multiArray = new int[n][];
+        for (int i = 0; i < n; i++) {
+            int size = Integer.parseInt(scanner.nextLine());
+            multiArray[i] = new int[size];
+        }
+    }
+}
