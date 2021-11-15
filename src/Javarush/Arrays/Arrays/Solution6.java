081d6dcecf9cef22f826cafe1db6d57285d5ec3b
Oleh Boholiub <boholiub95@gmail.com>
2021-11-15 19:28:16 +0100

    Solution6

---
+++ src/Javarush/Arrays/Arrays/Solution6.java
@@ -0,0 +1,20 @@
+package Javarush.Arrays.Arrays;
+
+/*
+Кубический калькулятор
+Let's write the realization of the calculator, which will consider the degree of numbers.
+To do this, create a Cube () method. As an argument, he must accept an integer value of Long type.
+The method should erect the value to the third degree and return it as the result of the method of the method.
+The numbers that have to operate can be large. Therefore, the Cube () method must return the type LONG.
+*/
+
+public class Solution6 {
+    public static void main(String[] args) {
+
+    }
+
+    public static long cube(long a){
+        return a*a*a;
+    }
+
+}
