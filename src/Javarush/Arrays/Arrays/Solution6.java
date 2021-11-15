ba98754a76c77a4f2aa59bce8a956600f5655f1a
Oleh Boholiub <boholiub95@gmail.com>
2021-11-15 19:58:30 +0100

    Solution

--- src/Javarush/Arrays/Arrays/Solution6.java
+++ src/Javarush/Arrays/Arrays/Solution6.java
@@ -18,3 +18,15 @@ public class Solution6 {
     }
 
 }
+/* public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public static long cube(long a){
+        return a*a*a;
+    }
+
+    public static long ninthDegree(long a){
+        return cube(cube(a));
+    }
+}*/
