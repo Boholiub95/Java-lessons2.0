06e05e676f5421d0fe585083e002398358b139a2
Oleh Boholiub <boholiub95@gmail.com>
2021-11-16 07:37:36 +0100

    Task1

---
+++ src/Javarush/Arrays/Arrays/Task1.java
@@ -0,0 +1,23 @@
+package Javarush.Arrays.Arrays;
+import java.util.Arrays;
+
+/*
+Массив значений
+Implement such filling in the fillArray method (array Integer [], int value).
+The method accepts an array and the value that needs to be filled in as arguments.
+The array is filled completely, regardless of its length.
+*/
+
+public class Task1 {
+    public static void main(String[] args) {
+        Integer[] array = new Integer[5];
+        fillArray(array, 3);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void fillArray(Integer[] array, int value){
+        for (int i = 0; i < array.length; i++) {
+            array[i] = value;
+        }
+    }
+}
