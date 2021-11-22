aefcc7329c2a66794d3526562514ff2bade18510
Oleh Boholiub <boholiub95@gmail.com>
2021-11-22 22:10:05 +0200

    flagPos

---
+++ src/Javarush/Arrays/Arrays/Solution11.java
@@ -0,0 +1,16 @@
+package Javarush.Arrays.Arrays;
+
+public class Solution11 {
+
+    public static int setFlag(int number, int flagPos) {
+        return number | (1 << flagPos);
+    }
+
+    public static int resetFlag(int number, int flagPos) {
+        return number & ~(1 << flagPos);
+    }
+
+    public static boolean checkFlag(int number, int flagPos) {
+        return (number & (1 << flagPos)) == (1 << flagPos);
+    }
+}
